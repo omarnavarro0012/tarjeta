a6cfcb81ceea8b008be68f871fc6d1a51cd8b6cf
omar navarro <omm27@hotmail.com>
2017-08-21 21:30:01 -0500

    Intercambio inicial de proyecto

---
+++ Tarjeta.java
@@ -0,0 +1,42 @@
+public class Tarjeta
+{
+    public float Saldo;
+    
+    public Tarjeta()
+    {
+        Saldo=0;
+    }
+    
+    public void Depositar(float dinero)
+    {
+        Saldo=Saldo+dinero;
+    }
+    
+    public float retirar(float dinero)
+    {
+        if (Saldo>=dinero)
+        {
+            Saldo=Saldo-dinero;
+            return dinero;
+        }
+        return 0;
+    
+    }
+    
+    public float consultar()
+    {
+        System.out.println("saldo="+Saldo+"pesos");
+        return 0;
+    }
+    
+    public float pago(float pago)
+    {
+        if(Saldo>=pago)
+        {
+            Saldo=Saldo-pago;
+        
+        }
+        return 0;
+        
+    }
+}
